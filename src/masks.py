9693798e8928f4e2891f57eb919b3734b9138704
Helga <_@example.com>
2025-04-20 20:18:55 +0300

    fixed

--- src/masks.py
+++ src/masks.py
@@ -3,7 +3,7 @@
 number_card = str(input("Введите номер карты"))
 
 
-def get_mask_card_number(number_card):
+def get_mask_card_number(number_card: str) -> str:
     masced_number = number_card[:6] + "*" * 6 + number_card[12:]
 
     return " ".join([masced_number[i : i + 4] for i in range(0, len(masced_number), 4)])
@@ -17,7 +17,7 @@ print(get_mask_card_number(number_card))
 number_card = str(input("Введите номер карты"))
 
 
-def get_mask_account(number_card):
+def get_mask_account(number_card: str) -> str:
     masced_account = "*" * 2 + number_card[-4:]
 
     return masced_account
--- src/widget.py
+++ src/widget.py
@@ -1,14 +1,13 @@
-from masks import get_mask_account, get_mask_card_number
-from masks import number_card
+from masks import get_mask_account, get_mask_card_number, number_card
 
 # импортируем функции
 
 
 # создаем функцию, принимающую название и номер карты или счета и маскирующую цифры
-def mask_account_card(number_card: str):
+def mask_account_card(number_card: str) -> str:
     # узнаем счет или карта и показываем только последние 4 цифры счета или первые 6 и последние 4 цифры карты
     if "Счет" in number_card:
-        number_score = "Счет" + "  " + "*" * 2 + number_card[-4:]
+        number_score = "Счет" + get_mask_account(number_card)
     else:
         split_string = number_card.split()
         for name in split_string:
@@ -16,10 +15,24 @@ def mask_account_card(number_card: str):
                 name_card = " ".join(split_string[:2])
             else:
                 name_card = " ".join(split_string[:1])
-        masced_number = split_string[-1][:6] + "*" * 6 + split_string[-1][12:]
+        masced_number = get_mask_card_number(number_card)
         number_score = name_card + " " + masced_number
 
     return number_score
 
 
 print(mask_account_card(number_card))
+
+
+# получаем дату и преобразуем в читаемый формат
+date_input = input("Введите дату")
+
+
+def get_date(date_input: str) -> str:
+    year = date_input[:4]
+    month = date_input[5:7]
+    day = date_input[8:10]
+    return f"{day}.{month}.{year}"
+
+
+print(get_date(date_input))
