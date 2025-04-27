1240f5ce600471144c51aef634582627d6d768b7
Helga <_@example.com>
2025-04-27 09:50:24 +0300

    init

--- src/masks.py
+++ src/masks.py
@@ -1,12 +1,12 @@
 # маскируем номер карты введенной клиентом, передавая на вывод первые 6 цифр и последние 4 цифры
 
-number_card = str(input("Введите номер карты"))
+number_card = input("Введите номер карты")
 
 
 def get_mask_card_number(number_card: str) -> str:
     masced_number = number_card[:6] + "*" * 6 + number_card[12:]
 
-    return " ".join([masced_number[i : i + 4] for i in range(0, len(masced_number), 4)])
+    return " ".join([masced_number[i: i + 4] for i in range(0, len(masced_number), 4)])
 
 
 print(get_mask_card_number(number_card))
