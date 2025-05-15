81c9976d2f155cd0da72d8b9a4c28a5ebb654179
Helga <_@example.com>
2025-05-15 14:09:39 +0300

    fixes for dev

--- src/masks.py
+++ src/masks.py
@@ -2,18 +2,21 @@
 
 
 def get_mask_card_number(number_card: str) -> str:
-    masced_number = number_card[:6] + "*" * 6 + number_card[12:]
+    if len(number_card) != 16 or not number_card.isdigit():
+        return "Неверно введен номер"
 
-    return " ".join([masced_number[i: i + 4] for i in range(0, len(masced_number), 4)])
+    masked_number = number_card[:6] + "*" * 6 + number_card[12:]
 
-
+    return " ".join([masked_number[i : i + 4] for i in range(0, len(masked_number), 4)])
 
 
 # маскируем номер карты, передавая на вывод последние 4 цифры
 
 
 def get_mask_account(number_card: str) -> str:
-    masced_account = "*" * 2 + number_card[-4:]
+    if len(number_card) != 20 or not number_card.isdigit():
+        return "Неверно введен номер"
 
-    return masced_account
+    masked_account = "*" * 2 + number_card[-4:]
 
+    return masked_account
