17d4cba7c89f769ce5f0ede3c57cfa49a85a1d8d
Helga <_@example.com>
2025-06-02 22:55:42 +0300

    fixes for dev

--- src/generators.py
+++ src/generators.py
@@ -24,16 +24,12 @@ def card_number_generator(start: int, end: int) -> Iterator[str]:
 """Функция принимает на вход список транзакций и возвращает значения по наименованию описания"""
 
 
-def transaction_descriptions(transactions: list[dict], descriptions: str) -> Iterator[str]:
-    """Проверяем наличие ключа в каждой транзакции"""
-    for i, transaction in enumerate(transactions):
-        if i >= 5:
-            break
+def transaction_descriptions(transactions: list[dict]) -> Iterator[str]:
+    """Получаем из словаря названия операций по ключу"""
+    for transaction in transactions:
+        if "description" in transaction:
+            yield transaction["description"]
 
-        if descriptions in transaction:
-            yield transaction[descriptions]
-        else:
-            yield "Нет описания операции"
 
 
 """Функция принимает на вход словари с транзакциями и выдает список отсортированный по валюте"""
